e99a8503a3dbcb28ddbb3bad99cafce03660b028
maisanazario <nazariomaisa@gmail.com>
2024-03-11 22:08:24 -0300

    correção do exercício_aula01

--- READEME.md
+++ READEME.md
@@ -8,3 +8,9 @@
     - `git --help`_verificar comandos disponíveis_
 - Primeiros comandos 
 
+## Primeiro exercício:
+- Criar um arquivo que dado o array de valores [2, 4, 3, 1, 6, 7, 9], retorne quais são pares. Fazer em JavaScript
+- Alterar o arquivo README.md
+- Gerar o commit
+- Enviar para o GitHub
+
--- exercicio_aula01.js
+++ exercicio_aula01.js
@@ -1,15 +1,21 @@
 
-function encontrarPares(array) {
-    let pares = [];
+// function encontrarPares(array) {
+//     let pares = [];
 
-for (let i = 0; i < Array.length; i++){
-    if (array[i] % 2 === 0) {
-        pares.push(array[i]);
-    }
-}
-return pares;
+// for (let i = 0; i < Array.length; i++){
+//     if (array[i] % 2 === 0) {
+//         pares.push(array[i]);
+//     }
+// }
+// return pares;
 
-}
-let valores = [2, 4, 3, 1, 6, 7, 9];
+// }
+// let valores = [2, 4, 3, 1, 6, 7, 9];
 
-console.log("Números pares:" encontrarPares(valores));
+// console.log("Números pares:" encontrarPares(valores));
+
+const array = [2, 4, 3, 1, 6, 7, 9];
+
+const pares = array.filter((item) => item %2 === 0);
+
+console.log (pares);
